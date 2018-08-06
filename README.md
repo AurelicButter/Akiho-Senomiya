8f382d08c87e17a3965f241051304fc3eae83e0d
Yvain Hoekstra <konirosen@protonmail.ch>
2018-08-06 02:56:31 -0400

    Initial Part Two

---
+++ README.md
@@ -0,0 +1,19 @@
+# Akiho Senomiya
+A Discord/Slack bot that relays messages posted by IFTTT Applet for Google Calendar notifications.
+
+<b>Prerequisites:</b>
+- Google account for Google Calendar.
+- IFTTT account with Google Calendar and Slack authorized.
+- A Slack account with a workspace for the bot to be on.
+- A Discord account with a bot in a shared server.
+
+<b>Introduction:</b>
+Much of this notification is dependent on the IFTTT applet. (See below)
+![IFTTT Applet Display](https://imgur.com/pfbl3xM)
+
+The bot will break or not display the information if it is not in that order or style. I was trying to keep the bot small and manageable as it's only job is to listen and transfer notifications. The message is then sent into the Slack DMs where it is processed into a Discord embed and transferred into your shared DMs on Discord for the event notification. Yes, you will get Slack notifications as well, but those can be turned off or ignored.
+
+*Name Origin:* Named after Akiho Senomiya from Robotics;Notes.
+
+<b>Self-hosting:</b>
+I will not support self-hosters. However, you are free to try and host it yourself. Good luck if you do!
---
+++ index.js
@@ -0,0 +1,38 @@
+const settings = require('./settings.json');
+const { Client, RichEmbed } = require('discord.js');
+const Slacker = require('slackbots');
+const { writeFile } = require("fs");
+
+const discord = new Client(); const slack = new Slacker(settings.slack);
+
+slack.on('start', function() { console.log("Slack services are online and functional!"); });
+slack.on('error', (err) => { writeFile('./error.txt', err, () => { console.log("Error was found on the Slack side and has been logged.") }); })
+slack.on('message', function(data) { 
+    if (data.type === 'message' && data.attachments) {
+        var event = data.attachments[0]; var text = []; var info = [12, 6, 7, 5];
+        text.push(event.text.search("Description:"), event.text.search("Where:"), event.text.search("Start:"), event.text.search("End:"));
+
+        var embed = new RichEmbed()
+            .setColor("#9a1bc9")
+            .setURL(event.title_link)
+            .setTitle(event.title);
+            if (text[0] !== -1) {
+                if (event.text.slice(text[0] + info[0], text[1]).trim().length > 2) { embed.setDescription(event.text.slice(text[0] + info[0], text[1])); } 
+                else { embed.setDescription("No Description was provided."); }
+            } if (text[1] !== -1) { 
+                if (event.text.slice(text[1] + info[1], text[2]).trim().length > 2) { embed.addField("Location:", event.text.slice(text[1] + info[1], text[2])); }
+                else { embed.addField("Location:", "Somewhere on Earth, I guess..."); }
+            } if (text[2] !== -1) { embed.addField("Start:", event.text.slice(text[2] + info[2], text[3]).trim(), true); }
+            if (text[3] !== -1) { embed.addField("End:", event.text.slice(text[3] + info[3]).trim(), true); }
+        
+        discord.users.get(settings.ownerID).send({embed}); 
+    } 
+});
+
+discord.on("ready", () => { 
+    discord.user.setActivity("Slack notifications", { type: "LISTENING" }); 
+    console.log("Discord services are online and functional!"); 
+});
+discord.on("error", (err) => { writeFile('./error.txt', err, () => { console.log("Error was found on the Discord side and has been logged.") }); });
+
+discord.login(settings.discordToken);
